78ad4619a6563861cfbfd84a9e8a3985d5274ad4
Saqib Razzaq <saqib.razzaq@virail.com>
2022-05-19 12:01:28 +0500

    Handler added with info

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# monolog-httpapi
-Send Monolog errors to external API (logstasah etc)
+
+
+monolog-httpapi
+=============
+
+HTTP API Handler for php monolog which allows you to log messages into external API of your choice.
+
+
+# Installation
+-----------
+Install using composer:
+
+```bash
+composer require sakydev/monolog-httpapi  
+```
+
+# Usage
+TODO
+
+# Examples
+Now Simply use it like this :
+
+```php
+require 'vendor/autoload.php';
+use Monolog\Logger;
+use sakydev\HttpApiHander\HttpApiHander;
+$log = new Logger('HttpApiHander');
+$headers = ['Content-Type: application/json'];
+$log->pushHandler(new HttpApiHander('http://url-here.com', $headers));
+
+
+$log->notice('hello world !');
+$log->info('hello world !');
+$log->debug('hello world !');
+$log->warning('hello world !');
+$log->critical('hello world !');
+$log->alert('hello world !');
+$log->emergency('hello world !');
+$log->error('hello world !');
+
+
+/**
+* Optionally you can pass second paramater such as a object
+**/
+$log->info('user just logged in !',['user'=>$user]);
+
+```
+
+# License
+This tool in Licensed under MIT, so feel free to fork it and make it better than it is !
---
+++ src/HttpApiHandler.php
@@ -0,0 +1,63 @@
+<?php
+namespace sakydev\HttpApiHandler;
+
+use Exception;
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Logger;
+use WpOrg\Requests\Requests;
+
+/**
+ * HTTP API Handler For Monolog
+ *
+ * This class allows you to send your logs to an external API 
+ *
+ * @author Saqib Razzaq <saqibrzzaq@gmail.com>
+ */
+
+class HttpApiLoggingHandler extends AbstractProcessingHandler
+{
+    public function __construct($apiUrl, $headers = ['Content-Type: application/json']) {
+        // Requests will be sent to apiUrl/{loglevel}/{channel}
+        $this->apiUrl = $apiUrl;
+        $this->headers = $headers;
+    }
+
+    /**
+     * format the log to send
+     * @param $record[] log data
+     * @return void
+     */
+    public function write(array $record): void
+    {
+        // If your api wants output data as a string, uncomment below lines
+
+        // $format = new LineFormatter;
+        // $context = $record['context'] ? $format->stringify($record['context']) : '';
+
+        // If you are sending to Kibana or something similar, it is better to send data as
+        // object so it is decodeable and makes more sense
+        $message = ['message' => $record['message'], 'extra' => $extra];
+        $this->send($record['level_name'], array_merge($message, $record['context']));
+    }
+
+    /**
+    * Send error messages to Logstash
+    * @author Saqib Razzaq
+    * @return void
+    */
+    public function send($level, $message, $channel = 'default-channel') : void {
+        try {
+            $url = sprintf("%s/%s/%s", $this->apiUrl, strtoupper($level), $channel);
+
+            $response = Requests::post($url, $this->headers, $message);
+            $response->throw_for_status();
+
+            if (empty($response->body)) {
+                throw new Exception("Failed to send logs to {$url}");
+            }
+        } catch (Exception $e) {
+            // do what you will with error
+        }
+    }
+}
